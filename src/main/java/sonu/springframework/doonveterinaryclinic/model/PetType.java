692686f533e2881293f8523335f3fa4f91813989
mrathore <mrathore@unomaha.edu>
2018-08-11 11:54:28 -0500

    Adding POJOs. closes #2.

---
+++ src/main/java/sonu/springframework/doonveterinaryclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package sonu.springframework.doonveterinaryclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
