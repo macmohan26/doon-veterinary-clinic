4bccfa0aa7ebd2047e64f6b967bc584a5af8320a
mrathore <mrathore@unomaha.edu>
2018-08-10 22:19:07 -0500

    Initial Project Add.

---
+++ src/main/java/sonu/springframework/doonveterinaryclinic/DoonVeterinaryClinicApplication.java
@@ -0,0 +1,12 @@
+package sonu.springframework.doonveterinaryclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoonVeterinaryClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoonVeterinaryClinicApplication.class, args);
+    }
+}
