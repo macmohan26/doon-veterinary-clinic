ebf629120cfb03c7ed9a3f21c24f02bdb76488f2
mrathore <mrathore@unomaha.edu>
2018-08-12 15:30:12 -0500

    Implement Map Based Service. closes #6

---
+++ veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,36 @@
+package sonu.springframework.doonveterinaryclinic.services.map;
+
+import sonu.springframework.doonveterinaryclinic.model.Owner;
+import sonu.springframework.doonveterinaryclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
