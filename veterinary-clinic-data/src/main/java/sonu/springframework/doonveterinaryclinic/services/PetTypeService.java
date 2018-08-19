18785450c3a26dcf6c6260fd2aa52fef10dd84cb
mrathore <mrathore@unomaha.edu>
2018-08-18 19:58:18 -0500

    Added PetTypeService.  closes #28

---
+++ veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package sonu.springframework.doonveterinaryclinic.services;
+
+import sonu.springframework.doonveterinaryclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package sonu.springframework.doonveterinaryclinic.services.map;
+
+import sonu.springframework.doonveterinaryclinic.model.PetType;
+import sonu.springframework.doonveterinaryclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
