e9984f7550378e45a5bff758a8c3ce819e4c181e
mrathore <mrathore@unomaha.edu>
2018-08-26 18:55:44 -0500

    Updated base entity with JPA annotations. Closes #35

--- veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/model/BaseEntity.java
+++ veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package sonu.springframework.doonveterinaryclinic.model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
