9b7dc657e3f81010cb357ce3f4fb86c20938405f
mrathore <mrathore@unomaha.edu>
2018-08-27 14:19:06 -0500

    Added Visit Entity, closes #38.

--- veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/model/Pet.java
+++ veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/model/Pet.java
@@ -2,6 +2,8 @@ package sonu.springframework.doonveterinaryclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table
@@ -20,6 +22,18 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
