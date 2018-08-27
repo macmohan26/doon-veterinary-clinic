e6357ca404a8da2cc2b875845c9f40815f2e41f9
mrathore <mrathore@unomaha.edu>
2018-08-26 19:37:23 -0500

    updated owner with JPA entity mappings. closes #36.

--- veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/model/Person.java
+++ veterinary-clinic-data/src/main/java/sonu/springframework/doonveterinaryclinic/model/Person.java
@@ -1,9 +1,19 @@
 package sonu.springframework.doonveterinaryclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
+
+    @Column(name = "first_name")
     public String getFirstName() {
         return firstName;
     }
